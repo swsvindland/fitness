b33b935cb5ba79bb76ce795ed19556529130b74d
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-10-27 13:43:34 -0400

    change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.svindland.fitness',
-    appName: 'Liftix',
+    appName: 'WorkoutTrack',
     webDir: 'dist',
     bundledWebRuntime: false,
 };
