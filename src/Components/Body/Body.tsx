15ee96277731b45d9700f628f0e29a0f8d3693a1
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-11-15 11:56:02 -0500

    fix chartjs font and color

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -4,9 +4,14 @@ import { WeightGraph } from './WeightGraph';
 import { BodyGraph } from './BodyGraph';
 import { BodyFatGraph } from './BodyFatGraph';
 import { useHideBackButton } from '../Navigation/headerHooks';
+import { Chart as ChartJS } from 'chart.js';
 
 export const Body: FC = () => {
     useHideBackButton();
+
+    ChartJS.defaults.color = '#AFD257';
+    ChartJS.defaults.font.family = "'Oswald', 'sans-serif'";
+    ChartJS.defaults.font.size = 10;
 
     return (
         <div className="max-w-2xl w-full">
