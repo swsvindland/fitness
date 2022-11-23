b74ede4acdc4cc6b820f66fce137613991a80fc5
Sam Svindland <sam@Sams-MBP.lan>
2022-11-23 11:33:59 -0600

    fix bad syntax

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './Components/App';
 import reportWebVitals from './reportWebVitals';
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
