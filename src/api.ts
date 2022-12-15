d866132f7711a999bfe4f341fedb6666354ef9f1
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-12-15 17:33:29 -0500

    release new version

--- src/api.ts
+++ src/api.ts
@@ -20,9 +20,9 @@ import { EdamamFoodHint } from './types/EdamamFoodHint';
 import { Food } from './types/Food';
 import { WorkoutExercise } from './types/WorkoutExercise';
 
-export const API_URL = 'http://localhost:7071';
+// export const API_URL = 'http://localhost:7071';
 // export const API_URL = 'https://fitness-dev.azurewebsites.net';
-// export const API_URL = 'https://fitness-prod.azurewebsites.net';
+export const API_URL = 'https://fitness-prod.azurewebsites.net';
 
 const getParams = (params?: object) => {
     const userId = localStorage.getItem('userId');
