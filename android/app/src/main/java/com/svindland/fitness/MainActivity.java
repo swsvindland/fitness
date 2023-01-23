979403c57764c0cbd3ff6780c1bdc9eaf1661e7a
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-23 18:12:33 -0500

    remove healthkit and google fit

--- android/app/src/main/java/com/svindland/fitness/MainActivity.java
+++ android/app/src/main/java/com/svindland/fitness/MainActivity.java
@@ -1,14 +1,4 @@
 package com.svindland.fitness;
-
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.adscientiam.capacitor.googlefit.GoogleFitPlugin;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        registerPlugin(GoogleFitPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity { }
