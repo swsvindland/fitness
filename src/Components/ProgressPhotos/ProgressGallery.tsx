e1829ccd109ec367c90b91e25337386de964421f
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-24 08:10:04 -0500

    add gallary dialog

--- src/Components/ProgressPhotos/ProgressGallery.tsx
+++ src/Components/ProgressPhotos/ProgressGallery.tsx
@@ -1,35 +1,50 @@
 import { useQuery } from '@tanstack/react-query';
 import { CDN_URL, getProgressPhotos } from '../../api';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { format } from 'date-fns';
 import { useShowBackButton } from '../Navigation/headerHooks';
+import { Viewer } from './Viewer';
 
 export const ProgressGallery: FC = () => {
     useShowBackButton();
     const photosQuery = useQuery(['ProgressPhotos'], getProgressPhotos);
     const photos = photosQuery.data?.data;
     const dates = Array.from(new Set(photos?.map((photo) => photo.created)));
+    const [open, setOpen] = useState(false);
+    const [openedImage, setOpenedImage] = useState<string>('');
 
     return (
-        <div className="max-w-2xl w-full grid grid-cols-1">
-            {dates.map((date) => (
-                <div>
-                    <h2 className="text-lg text-secondary my-2">
-                        {format(new Date(date), 'PP')}
-                    </h2>
-                    <div className="grid grid-cols-3 gap-4">
-                        {photos
-                            ?.filter((item) => item.created === date)
-                            .map((photo) => (
-                                <img
-                                    className="rounded shadow"
-                                    src={`${CDN_URL}${photo.filename}`}
-                                    alt=""
-                                />
-                            ))}
+        <>
+            <div className="max-w-2xl w-full grid grid-cols-1">
+                {dates.map((date) => (
+                    <div>
+                        <h2 className="text-lg text-secondary my-2">
+                            {format(new Date(date), 'PP')}
+                        </h2>
+                        <div className="grid grid-cols-3 gap-4">
+                            {photos
+                                ?.filter((item) => item.created === date)
+                                .map((photo) => (
+                                    <button
+                                        onClick={() => {
+                                            setOpen(true);
+                                            setOpenedImage(
+                                                `${CDN_URL}${photo.filename}`
+                                            );
+                                        }}
+                                    >
+                                        <img
+                                            className="rounded shadow"
+                                            src={`${CDN_URL}${photo.filename}`}
+                                            alt=""
+                                        />
+                                    </button>
+                                ))}
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+            <Viewer open={open} setOpen={setOpen} image={openedImage} />
+        </>
     );
 };
--- src/Components/ProgressPhotos/ProgressPhotos.tsx
+++ src/Components/ProgressPhotos/ProgressPhotos.tsx
@@ -1,13 +1,16 @@
-import { FC, useContext } from 'react';
+import { FC, useContext, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { CDN_URL, getProgressPhotos } from '../../api';
 import { LinkButton } from '../Buttons/LinkButton';
 import { LinkSecondaryButton } from '../Buttons/LinkSecondaryButton';
 import { AuthContext } from '../Auth/Auth';
 import { Button } from '../Buttons/Button';
+import { Viewer } from './Viewer';
 
 export const ProgressPhotos: FC = () => {
     const { user, setOpenPurchase } = useContext(AuthContext);
+    const [open, setOpen] = useState(false);
+    const [openedImage, setOpenedImage] = useState<string>('');
     const photosQuery = useQuery(['ProgressPhotos'], getProgressPhotos);
 
     if (!user?.paid) {
@@ -26,26 +29,36 @@ export const ProgressPhotos: FC = () => {
     }
 
     return (
-        <div className="card p-4 w-full">
-            <h2 className="text-lg text-secondary my-4">Progress Photos</h2>
-            <div className="flex flex-row">
-                <LinkButton to="body/progress" className="mr-2">
-                    See All
-                </LinkButton>
-                <LinkSecondaryButton to="body/progress/upload">
-                    Upload
-                </LinkSecondaryButton>
+        <>
+            <div className="card p-4 w-full">
+                <h2 className="text-lg text-secondary my-4">Progress Photos</h2>
+                <div className="flex flex-row">
+                    <LinkButton to="body/progress" className="mr-2">
+                        See All
+                    </LinkButton>
+                    <LinkSecondaryButton to="body/progress/upload">
+                        Upload
+                    </LinkSecondaryButton>
+                </div>
+                <div className="grid grid-cols-4 gap-4 mt-2">
+                    {photosQuery.data?.data.slice(0, 4).map((photo) => (
+                        <button
+                            onClick={() => {
+                                setOpen(true);
+                                setOpenedImage(`${CDN_URL}${photo.filename}`);
+                            }}
+                        >
+                            <img
+                                className="rounded shadow"
+                                src={`${CDN_URL}${photo.filename}`}
+                                alt=""
+                                width={150}
+                            />
+                        </button>
+                    ))}
+                </div>
             </div>
-            <div className="grid grid-cols-4 gap-4 mt-2">
-                {photosQuery.data?.data.slice(0, 4).map((photo) => (
-                    <img
-                        className="rounded shadow"
-                        src={`${CDN_URL}${photo.filename}`}
-                        alt=""
-                        width={150}
-                    />
-                ))}
-            </div>
-        </div>
+            <Viewer open={open} setOpen={setOpen} image={openedImage} />
+        </>
     );
 };
