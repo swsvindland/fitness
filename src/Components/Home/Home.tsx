1f1a1d6e8dc3fad3eb90d8b51a040bdbf405c809
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-28 13:36:54 -0500

    add quick buttons for food

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -4,18 +4,40 @@ import { Todo } from './Todo';
 import { BodyFatGraph } from '../Body/BodyFatGraph';
 import { MacroGrid } from '../Macros/MacroGrid';
 import { FirebaseAnalytics } from '@capacitor-community/firebase-analytics';
+import { SecondaryButton } from '../Buttons/SecondaryButton';
+import { LinkButton } from '../Buttons/LinkButton';
+import { useHistory } from 'react-router-dom';
 
 export const Home: FC = () => {
     useHideBackButton();
+    const history = useHistory();
 
     FirebaseAnalytics.setScreenName({
         screenName: 'home',
         nameOverride: 'HomeScreen',
     });
 
+    const handleStartScan = () => {
+        history.push('/scanner');
+    };
+
     return (
         <div className="grid w-full max-w-2xl grid-cols-1">
-            <MacroGrid />
+            <MacroGrid home />
+            <div className="my-2 flex flex-row justify-between">
+                <SecondaryButton
+                    className="mr-1 flex w-full justify-center"
+                    onClick={handleStartScan}
+                >
+                    Scan Barcode
+                </SecondaryButton>
+                <LinkButton
+                    to="/eat/add-food"
+                    className="ml-1 flex w-full justify-center"
+                >
+                    Add Food
+                </LinkButton>
+            </div>
             <Todo />
             <BodyFatGraph />
         </div>
--- src/Components/Macros/MacroGrid.tsx
+++ src/Components/Macros/MacroGrid.tsx
@@ -6,7 +6,11 @@ import { getCurrentUserMacros, getMacros } from '../../api';
 import { Units } from '../../types/user';
 import { AuthContext } from '../Auth/Auth';
 
-export const MacroGrid: FC = () => {
+interface IProps {
+    home?: boolean;
+}
+
+export const MacroGrid: FC<IProps> = ({ home }) => {
     const { user } = useContext(AuthContext);
     const macrosQuery = useQuery(['Macros'], () => {
         return getMacros();
@@ -29,44 +33,48 @@ export const MacroGrid: FC = () => {
                     amountHigh={macrosQuery.data?.data.caloriesHigh}
                     currentAmount={currentMacrosQuery.data?.data.calories}
                     unit={user?.unit === Units.Imperial ? 'Cal' : 'kcal'}
-                    customMacros={true}
+                    customMacros={!home}
                 />
-                <dl className="mt-2 grid grid-cols-2 gap-2 overflow-hidden sm:grid-cols-4">
-                    <MacroGridUnit
-                        name="Protein"
-                        amount={macrosQuery.data?.data?.protein ?? 0}
-                        amountHigh={macrosQuery.data?.data.proteinHigh}
-                        currentAmount={
-                            currentMacrosQuery.data?.data?.protein ?? 0
-                        }
-                        unit="g"
-                    />
-                    <MacroGridUnit
-                        name="Fat"
-                        amount={macrosQuery.data?.data?.fat ?? 0}
-                        amountHigh={macrosQuery.data?.data.fatHigh}
-                        currentAmount={currentMacrosQuery.data?.data?.fat ?? 0}
-                        unit="g"
-                    />
-                    <MacroGridUnit
-                        name="Carbs"
-                        amount={macrosQuery.data?.data?.carbs ?? 0}
-                        amountHigh={macrosQuery.data?.data.carbsHigh}
-                        currentAmount={
-                            currentMacrosQuery.data?.data?.carbs ?? 0
-                        }
-                        unit="g"
-                    />
-                    <MacroGridUnit
-                        name="Fiber"
-                        amount={macrosQuery.data?.data?.fiber ?? 0}
-                        amountHigh={macrosQuery.data?.data.fiberHigh}
-                        currentAmount={
-                            currentMacrosQuery.data?.data?.fiber ?? 0
-                        }
-                        unit="g"
-                    />
-                </dl>
+                {!home ? (
+                    <dl className="mt-2 grid grid-cols-2 gap-2 overflow-hidden sm:grid-cols-4">
+                        <MacroGridUnit
+                            name="Protein"
+                            amount={macrosQuery.data?.data?.protein ?? 0}
+                            amountHigh={macrosQuery.data?.data.proteinHigh}
+                            currentAmount={
+                                currentMacrosQuery.data?.data?.protein ?? 0
+                            }
+                            unit="g"
+                        />
+                        <MacroGridUnit
+                            name="Fat"
+                            amount={macrosQuery.data?.data?.fat ?? 0}
+                            amountHigh={macrosQuery.data?.data.fatHigh}
+                            currentAmount={
+                                currentMacrosQuery.data?.data?.fat ?? 0
+                            }
+                            unit="g"
+                        />
+                        <MacroGridUnit
+                            name="Carbs"
+                            amount={macrosQuery.data?.data?.carbs ?? 0}
+                            amountHigh={macrosQuery.data?.data.carbsHigh}
+                            currentAmount={
+                                currentMacrosQuery.data?.data?.carbs ?? 0
+                            }
+                            unit="g"
+                        />
+                        <MacroGridUnit
+                            name="Fiber"
+                            amount={macrosQuery.data?.data?.fiber ?? 0}
+                            amountHigh={macrosQuery.data?.data.fiberHigh}
+                            currentAmount={
+                                currentMacrosQuery.data?.data?.fiber ?? 0
+                            }
+                            unit="g"
+                        />
+                    </dl>
+                ) : null}
             </div>
         </div>
     );
