0918a493b97a174bfaba9ff5d30c6372cb580458
Sam Svindland <swsvindland@gmail.com>
2023-11-11 21:26:15 -0500

    hopefully fix sw with next app router

--- apps/workout-t3/next.config.mjs
+++ apps/workout-t3/next.config.mjs
@@ -1,6 +1,29 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
 import pwa from "next-pwa";
+import path from 'node:path'
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const generateAppDirEntry = (entry) => {
+    // const packageDirectory = path.dirname("node_modules/next-pwa/");
+    // const registerJs = path.join(packageDirectory, "register.js");
+    const registerJs = "node_modules/next-pwa/register.js"
+
+    return entry().then((entries) => {
+        // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+        if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+            if (Array.isArray(entries["main-app"])) {
+                entries["main-app"].unshift(registerJs);
+            } else if (typeof entries["main-app"] === "string") {
+                entries["main-app"] = [registerJs, entries["main-app"]];
+            }
+        }
+
+        return entries;
+    });
+
+};
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -10,12 +33,20 @@ const config = {
     /** We already do linting and typechecking as separate tasks in CI */
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
+
+    webpack(config) {
+        if( !isDev ){
+            const entry = generateAppDirEntry(config.entry);
+            config.entry = () => entry;
+        }
+        return config;
+    },
 };
 
 const withPWA = pwa({
     dest: 'public',
     register: true,
-    swSrc: 'src/service-worker.js'
+    disable: isDev,
 });
 
 export default withPWA({
