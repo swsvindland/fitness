4a912db861d84653cefec4d45d6d86e35f73edeb
Sam Svindland <swsvindland@gmail.com>
2023-12-16 12:40:27 +0900

    disable text field zoom

--- apps/workout-t3/src/app/layout.tsx
+++ apps/workout-t3/src/app/layout.tsx
@@ -25,6 +25,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
     width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
     viewportFit: 'cover',
     themeColor: '#0D3140',
 };
