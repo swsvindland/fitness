0f6c104a6c0c69043a72d7d3205cd48871cdb509
Sam Svindland <swsvindland@gmail.com>
2024-01-07 14:33:36 -0500

    update theme

--- apps/blood-pressure/tailwind.config.ts
+++ apps/blood-pressure/tailwind.config.ts
@@ -15,6 +15,10 @@ export default {
                 light: {},
                 dark: {
                     colors: {
+                        content1: '#154255',
+                        content2: '#154255',
+                        content3: '#154255',
+                        content4: '#154255',
                         background: '#0D3140',
                         foreground: '#F7C619',
                         primary: {
@@ -30,6 +34,21 @@ export default {
                             50: '#F2F8F7',
                             DEFAULT: '#28827A',
                             foreground: '#F7C619',
+                        },
+                        success: {
+                            500: '#07B561',
+                            DEFAULT: '#07B561',
+                            foreground: '#fff',
+                        },
+                        warning: {
+                            500: '#FFD400',
+                            DEFAULT: '#FFD400',
+                            foreground: '#0D3140',
+                        },
+                        danger: {
+                            500: '#DB255E',
+                            DEFAULT: '#DB255E',
+                            foreground: '#fff',
                         },
                         focus: '#F7C619',
                         secondary: '#AFD257',
