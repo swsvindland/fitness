d69e7cefc8f9b5b768b1d1bbb810c18f258381e3
Sam Svindland <swsvindland@gmail.com>
2024-01-07 14:06:19 -0500

    add theme provider

--- apps/blood-pressure/src/app/providers.tsx
+++ apps/blood-pressure/src/app/providers.tsx
@@ -3,7 +3,14 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function Providers({ children }: { children: ReactNode }) {
-    return <NextUIProvider>{children}</NextUIProvider>;
+    return (
+        <NextUIProvider>
+            <NextThemesProvider attribute="class" defaultTheme="dark">
+                {children}
+            </NextThemesProvider>
+        </NextUIProvider>
+    );
 }
