b138e630c8419ce48b55f5e3e7b640416640e216
Sam Svindland <swsvindland@gmail.com>
2024-01-15 22:01:43 -0500

    update grid unit

--- apps/macro/src/app/_components/Macros/MacroGridUnit.tsx
+++ apps/macro/src/app/_components/Macros/MacroGridUnit.tsx
@@ -1,4 +1,11 @@
 import { FC } from 'react';
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Progress,
+} from '@nextui-org/react';
 
 interface IProps {
     name: string;
@@ -16,49 +23,36 @@ export const MacroGridUnit: FC<IProps> = ({
     const percentage = ((currentAmount ?? 0) / amount) * 100;
 
     return (
-        <div className="card px-4 py-5 sm:p-6">
-            <dt className="text-secondary flex justify-between text-base font-normal">
+        <Card className="p-4">
+            <CardHeader className="text-secondary flex justify-between text-base font-normal">
                 <span>{name}</span>
-            </dt>
-            <dd className="mt-1 flex items-baseline">
-                {currentAmount !== undefined ? (
-                    <>
-                        <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
-                            {currentAmount?.toFixed(0) ?? 0}
-                        </span>
-                        <span className="text-ternary text-xs sm:text-lg">
-                            {unit}
-                        </span>
-                        <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
-                            /
-                        </span>
-                    </>
-                ) : null}
-                <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
-                    {amount?.toFixed(0)}
-                </span>
-                <span className="text-ternary text-xs sm:text-lg">{unit}</span>
-            </dd>
-            {currentAmount !== undefined ? (
-                <div className="relative mt-4">
-                    <div className="bg-secondary-light mb-4 flex h-2 overflow-hidden rounded text-xs shadow-inner">
-                        <div
-                            style={{
-                                width: `${
-                                    ((currentAmount ?? 0) / amount) * 100
-                                }%`,
-                            }}
-                            className={`flex flex-col justify-center whitespace-nowrap text-center text-white shadow-inner ${
-                                percentage < 95
-                                    ? 'bg-secondary'
-                                    : percentage > 105
-                                      ? 'bg-error'
-                                      : 'bg-primary'
-                            }`}
-                        ></div>
-                    </div>
-                </div>
-            ) : null}
-        </div>
+            </CardHeader>
+            <CardBody>
+                <dd className="my-1 flex items-baseline">
+                    {currentAmount !== undefined ? (
+                        <>
+                            <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
+                                {currentAmount?.toFixed(0) ?? 0}
+                            </span>
+                            <span className="text-ternary text-xs sm:text-lg">
+                                {unit}
+                            </span>
+                            <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
+                                /
+                            </span>
+                        </>
+                    ) : null}
+                    <span className="text-ternary flex items-baseline text-lg font-semibold sm:text-2xl">
+                        {amount?.toFixed(0)}
+                    </span>
+                    <span className="text-ternary text-xs sm:text-lg">
+                        {unit}
+                    </span>
+                </dd>
+            </CardBody>
+            <CardFooter>
+                <Progress value={percentage} />
+            </CardFooter>
+        </Card>
     );
 };
