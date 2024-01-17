d36af943bcb39d71e77a9fddf579049a254c896c
Sam Svindland <swsvindland@gmail.com>
2024-01-17 18:26:25 -0500

    fix macro track build

--- apps/macro/src/app/custom-macros/page.tsx
+++ apps/macro/src/app/custom-macros/page.tsx
@@ -1,6 +1,6 @@
 import { CustomMacroForm } from '~/app/_components/Macros/CustomMacroForm';
-import { LoadingPage } from '~/app/_components/Loading/LoadingPage';
 import { Suspense } from 'react';
+import { LoadingPage } from '@fitness/ui';
 
 export default async function Page() {
     return (
