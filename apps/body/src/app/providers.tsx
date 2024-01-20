7e75d39005e564fe86fc9fe16e0ffbc020260968
Sam Svindland <swsvindland@gmail.com>
2024-01-20 12:52:45 -0500

    fix ports:

--- apps/body/src/app/providers.tsx
+++ apps/body/src/app/providers.tsx
@@ -1,9 +1,14 @@
-// app/providers.tsx
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { Chart as ChartJS } from 'chart.js';
+
+ChartJS.defaults.color = '#AFD257';
+ChartJS.defaults.borderColor = '#0D3140';
+ChartJS.defaults.font.family = "'Oswald', 'sans-serif'";
+ChartJS.defaults.font.size = 10;
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
--- apps/macro/src/app/providers.tsx
+++ apps/macro/src/app/providers.tsx
@@ -1,10 +1,15 @@
-// app/providers.tsx
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { useRouter } from 'next/navigation';
+import { Chart as ChartJS } from 'chart.js';
+
+ChartJS.defaults.color = '#AFD257';
+ChartJS.defaults.borderColor = '#0D3140';
+ChartJS.defaults.font.family = "'Oswald', 'sans-serif'";
+ChartJS.defaults.font.size = 10;
 
 export function Providers({ children }: { children: ReactNode }) {
     const router = useRouter();
