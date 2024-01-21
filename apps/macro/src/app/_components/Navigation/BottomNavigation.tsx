f6668ea804b80f63d0ca831a4f390b3c6c9e8820
Sam Svindland <swsvindland@gmail.com>
2024-01-21 15:01:44 -0500

    update bottom nav

--- apps/macro/src/app/_components/Navigation/BottomNavigation.tsx
+++ apps/macro/src/app/_components/Navigation/BottomNavigation.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation';
 import { isSelected } from '~/app/_components/Navigation/utils';
 import { Cog6ToothIcon, HomeIcon } from '@heroicons/react/24/outline';
 import { Card } from '@nextui-org/react';
+import { Button } from '@nextui-org/button';
 
 export const BottomNavigation: FC = () => {
     const pathname = usePathname();
@@ -22,20 +23,39 @@ export const BottomNavigation: FC = () => {
             isSelected(pathname, route) ? 'text-ternary' : 'text-secondary'
         } tab tab-home block text-xs w-6 h-6 m-auto`;
 
+    const color = (route: string[]) =>
+        isSelected(pathname, route) ? 'primary' : 'secondary';
+
     return (
         <Card
             id="bottom-navigation"
             className="pb-safe fixed inset-x-0 bottom-0 z-50 block rounded-none"
         >
-            <div id="tabs" className="flex justify-between">
-                <Link href="/" className={linkStyle(['/'])}>
-                    <HomeIcon className={iconStyle(['/'])} />
-                    <span className="tab tab-home block text-xs">Home</span>
-                </Link>
-                <Link href="/settings" className={linkStyle(['/settings'])}>
-                    <Cog6ToothIcon className={iconStyle(['/settings'])} />
-                    <span className="tab tab-home block text-xs">Settings</span>
-                </Link>
+            <div id="tabs" className="grid h-full grid-cols-2">
+                <Button
+                    color={color(['/'])}
+                    variant="light"
+                    as={Link}
+                    href="/"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <HomeIcon className="h-6" />
+                        <span>Home</span>
+                    </div>
+                </Button>
+                <Button
+                    color={color(['/settings'])}
+                    variant="light"
+                    as={Link}
+                    href="/settings"
+                    className="h-16"
+                >
+                    <div className="flex flex-col items-center justify-center">
+                        <Cog6ToothIcon className="h-6" />
+                        <span>Settings</span>
+                    </div>
+                </Button>
             </div>
         </Card>
     );
