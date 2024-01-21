c9893e1f2e1ad3ac32574fdcc61ba01861f4808b
Sam Svindland <swsvindland@gmail.com>
2024-01-21 17:39:37 -0500

    update header icon

--- apps/supplement/src/app/_components/Navigation/Header.tsx
+++ apps/supplement/src/app/_components/Navigation/Header.tsx
@@ -3,7 +3,7 @@
 import { FC } from 'react';
 import { UserButton } from '@clerk/nextjs';
 import { Card } from '@nextui-org/react';
-import { Dumbbell } from '@fitness/ui';
+import { Capsule, Dumbbell } from '@fitness/ui';
 
 export const Header: FC = () => {
     return (
@@ -14,7 +14,7 @@ export const Header: FC = () => {
             <div className="mx-auto px-4 sm:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="flex flex-1 items-center">
-                        <Dumbbell className="fill-secondary h-10 w-10" />
+                        <Capsule className="fill-secondary h-10 w-10" />
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <UserButton afterSignOutUrl="/" />
