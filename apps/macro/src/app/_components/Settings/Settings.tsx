d85ed4c04d466fda46d662e41313a58e91be13d6
Sam Svindland <swsvindland@gmail.com>
2024-01-21 16:30:53 -0500

    make settings page async

--- apps/macro/src/app/_components/Settings/Settings.tsx
+++ apps/macro/src/app/_components/Settings/Settings.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { LocalizationSettings } from '~/app/_components/Settings/LocalizationSettings';
 import { MealSettings } from '~/app/_components/Settings/MealSettings';
 
-export const Settings: FC = () => {
+export const Settings: FC = async () => {
     return (
         <div className="container flex flex-col gap-4">
             <LocalizationSettings />
