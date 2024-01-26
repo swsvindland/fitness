16adf2b533655b234c3ecd45ca3733567118b3eb
Sam Svindland <swsvindland@gmail.com>
2024-01-26 08:38:06 -0500

    setup body track settings

--- apps/body/src/app/_components/Settings/UnitsForm.tsx
+++ apps/body/src/app/_components/Settings/UnitsForm.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import { FC, FormEvent, useEffect, useState } from 'react';
-import { Button } from '../Buttons/Button';
-import { SecondaryButton } from '../Buttons/SecondaryButton';
 import { Units } from '@fitness/types';
 import { api } from '~/trpc/react';
-import { useRouter } from 'next/navigation';
+import { Button } from '@nextui-org/button';
+import { Radio, RadioGroup } from '@nextui-org/react';
 
-export const UnitsForm: FC = () => {
+interface IProps {
+    onClose: () => void;
+}
+
+export const UnitsForm: FC<IProps> = (props) => {
     const [unit, setUnit] = useState<string>(Units.Imperial);
     const utils = api.useUtils();
-    const router = useRouter();
 
     const settings = api.settings.getUserSettings.useQuery();
 
@@ -31,7 +33,7 @@ export const UnitsForm: FC = () => {
         if (!settings.data) return;
 
         mutation.mutate({ id: Number(settings.data.Id), unit });
-        router.back();
+        props.onClose();
     };
 
     const handleClear = () => {
@@ -41,62 +43,24 @@ export const UnitsForm: FC = () => {
     return (
         <div className="container">
             <div className="mt-5 md:col-span-2 md:mt-0">
-                <p className="text-ternary mb-4 text-center">
-                    It is recommended that you do not change once you add a
-                    bunch of data. It will not update past data entry, just how
-                    labels and certain computations, such as body fat, are
-                    computed
-                </p>
                 <form onSubmit={handleSubmit}>
-                    <div className="card overflow-hidden rounded shadow">
-                        <div className="p-4">
-                            <div className="flex items-center">
-                                <input
-                                    id="imperial"
-                                    name="imperial"
-                                    type="radio"
-                                    checked={unit === Units.Imperial}
-                                    onChange={(event) => {
-                                        if (event.target.checked) {
-                                            setUnit(Units.Imperial);
-                                        }
-                                    }}
-                                    className="border-ternary accent-secondary h-4 w-4"
-                                />
-                                <label
-                                    htmlFor="sex-male"
-                                    className="text-ternary ml-3 block text-sm font-medium"
-                                >
-                                    Imperial
-                                </label>
-                            </div>
-                            <div className="flex items-center">
-                                <input
-                                    id="metric"
-                                    name="metric"
-                                    type="radio"
-                                    checked={unit === Units.Metric}
-                                    onChange={(event) => {
-                                        if (event.target.checked) {
-                                            setUnit(Units.Metric);
-                                        }
-                                    }}
-                                    className="border-ternary accent-secondary h-4 w-4"
-                                />
-                                <label
-                                    htmlFor="sex-female"
-                                    className="text-ternary ml-3 block text-sm font-medium"
-                                >
-                                    Metric
-                                </label>
-                            </div>
-                        </div>
-                        <div className="bg-primary-dark dark:bg-background flex justify-between px-4 py-3 text-right sm:px-6">
-                            <SecondaryButton onClick={handleClear}>
-                                Clear
-                            </SecondaryButton>
-                            <Button type="submit">Save</Button>
-                        </div>
+                    <div className="pb-6">
+                        <RadioGroup
+                            label="It is recommended that you do not change once you add a bunch of data. It will not update past data entry, just how labels and certain computations, such as body fat, are computed"
+                            value={unit}
+                            onValueChange={setUnit}
+                        >
+                            <Radio value="Imperial">Imperial</Radio>
+                            <Radio value="Metric">Metric</Radio>
+                        </RadioGroup>
+                    </div>
+                    <div className="flex justify-between pb-3">
+                        <Button onClick={handleClear} color="warning">
+                            Clear
+                        </Button>
+                        <Button type="submit" color="primary">
+                            Save
+                        </Button>
                     </div>
                 </form>
             </div>
