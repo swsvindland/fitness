8410bd34556d3cd850dcf2c4522fda49c23c8e78
Sam Svindland <swsvindland@gmail.com>
2024-01-28 15:18:12 -0500

    update nav buttons on body track

--- apps/body/src/app/_components/Navigation/BottomNavigation.tsx
+++ apps/body/src/app/_components/Navigation/BottomNavigation.tsx
@@ -12,21 +12,13 @@ import {
     ScaleIcon,
 } from '@heroicons/react/24/outline';
 import { Card } from '@nextui-org/react';
+import { Button } from '@nextui-org/button';
 
 export const BottomNavigation: FC = () => {
     const pathname = usePathname();
 
-    const linkStyle = (route: string[]) =>
-        `${
-            isSelected(pathname, route)
-                ? 'text-ternary bg-primary-dark'
-                : 'text-secondary'
-        } w-full justify-center inline-block text-center py-3 rounded hover:bg-primary-dark active:bg-primary-dark`;
-
-    const iconStyle = (route: string[]) =>
-        `${
-            isSelected(pathname, route) ? 'text-ternary' : 'text-secondary'
-        } tab tab-home block text-xs w-6 h-6 m-auto`;
+    const color = (route: string[]) =>
+        isSelected(pathname, route) ? 'primary' : 'secondary';
 
     return (
         <Card
@@ -34,26 +26,66 @@ export const BottomNavigation: FC = () => {
             className="pb-safe fixed inset-x-0 bottom-0 z-50 block rounded-none"
         >
             <div id="tabs" className="flex justify-between">
-                <Link href="/" className={linkStyle(['/'])}>
-                    <HomeIcon className={iconStyle(['/'])} />
-                    <span className="tab tab-home block text-xs">Home</span>
-                </Link>
-                <Link href="/weights" className={linkStyle(['/weights'])}>
-                    <ScaleIcon className={iconStyle(['/weights'])} />
-                    <span className="tab tab-home block text-xs">Weights</span>
-                </Link>
-                <Link href="/heights" className={linkStyle(['/heights'])}>
-                    <ArrowUpCircleIcon className={iconStyle(['/heights'])} />
-                    <span className="tab tab-home block text-xs">Heights</span>
-                </Link>
-                <Link href="/bodies" className={linkStyle(['/bodies'])}>
-                    <ArrowsRightLeftIcon className={iconStyle(['/bodies'])} />
-                    <span className="tab tab-home block text-xs">Bodies</span>
-                </Link>
-                <Link href="/settings" className={linkStyle(['/settings'])}>
-                    <Cog6ToothIcon className={iconStyle(['/settings'])} />
-                    <span className="tab tab-home block text-xs">Settings</span>
-                </Link>
+                <Button
+                    color={color(['/'])}
+                    variant="light"
+                    as={Link}
+                    href="/"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <HomeIcon className="h-6" />
+                        <span>Home</span>
+                    </div>
+                </Button>
+                <Button
+                    color={color(['/weights'])}
+                    variant="light"
+                    as={Link}
+                    href="/weights"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <ScaleIcon className="h-6" />
+                        <span>Weights</span>
+                    </div>
+                </Button>
+                <Button
+                    color={color(['/heights'])}
+                    variant="light"
+                    as={Link}
+                    href="/heights"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <ArrowUpCircleIcon className="h-6" />
+                        <span>Height</span>
+                    </div>
+                </Button>
+                <Button
+                    color={color(['/bodies'])}
+                    variant="light"
+                    as={Link}
+                    href="/bodies"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <ArrowsRightLeftIcon className="h-6" />
+                        <span>Bodies</span>
+                    </div>
+                </Button>
+                <Button
+                    color={color(['/settings'])}
+                    variant="light"
+                    as={Link}
+                    href="/settings"
+                    className="h-16"
+                >
+                    <div className="my-4 flex flex-col items-center justify-center">
+                        <Cog6ToothIcon className="h-6" />
+                        <span>Settings</span>
+                    </div>
+                </Button>
             </div>
         </Card>
     );
--- apps/body/src/app/_components/Navigation/Sidebar.tsx
+++ apps/body/src/app/_components/Navigation/Sidebar.tsx
@@ -12,50 +12,78 @@ import {
     ScaleIcon,
 } from '@heroicons/react/24/outline';
 import { Card } from '@nextui-org/react';
+import { Button } from '@nextui-org/button';
 
 export const Sidebar: FC = () => {
     const pathname = usePathname();
 
-    const linkStyle = (route: string[]) =>
-        `${
-            isSelected(pathname, route)
-                ? 'text-ternary bg-primary-dark'
-                : 'text-secondary'
-        } w-full justify-center inline-block text-center py-4 rounded hover:bg-primary-dark active:bg-primary-dark`;
-
-    const iconStyle = (route: string[]) =>
-        `${
-            isSelected(pathname, route) ? 'text-ternary' : 'text-secondary'
-        } tab tab-home block text-xs w-6 h-6 m-auto`;
+    const color = (route: string[]) =>
+        isSelected(pathname, route) ? 'primary' : 'secondary';
 
     return (
         <Card className="pl-safe z-20 flex min-h-0 flex-1 flex-col rounded-none">
             <div className="flex flex-1 flex-col overflow-y-auto pb-4 pt-5">
-                <nav className="mt-16 flex-1 space-y-1 bg-transparent px-2">
-                    <Link href="/" className={linkStyle(['/'])}>
-                        <HomeIcon className={iconStyle(['/'])} />
-                        Home
-                    </Link>
-                    <Link href="/weights" className={linkStyle(['/weights'])}>
-                        <ScaleIcon className={iconStyle(['/weights'])} />
-                        Weights
-                    </Link>
-                    <Link href="/heights" className={linkStyle(['/heights'])}>
-                        <ArrowUpCircleIcon
-                            className={iconStyle(['/heights'])}
-                        />
-                        Heights
-                    </Link>
-                    <Link href="/bodies" className={linkStyle(['/bodies'])}>
-                        <ArrowsRightLeftIcon
-                            className={iconStyle(['/bodies'])}
-                        />
-                        Bodies
-                    </Link>
-                    <Link href="/settings" className={linkStyle(['/settings'])}>
-                        <Cog6ToothIcon className={iconStyle(['/settings'])} />
-                        Settings
-                    </Link>
+                <nav className="mt-16 flex flex-col justify-center space-y-1">
+                    <Button
+                        color={color(['/'])}
+                        variant="light"
+                        as={Link}
+                        href="/"
+                        className="h-16"
+                    >
+                        <div className="my-4 flex flex-col items-center justify-center">
+                            <HomeIcon className="h-6" />
+                            <span>Home</span>
+                        </div>
+                    </Button>
+                    <Button
+                        color={color(['/weights'])}
+                        variant="light"
+                        as={Link}
+                        href="/weights"
+                        className="h-16"
+                    >
+                        <div className="my-4 flex flex-col items-center justify-center">
+                            <ScaleIcon className="h-6" />
+                            <span>Weights</span>
+                        </div>
+                    </Button>
+                    <Button
+                        color={color(['/heights'])}
+                        variant="light"
+                        as={Link}
+                        href="/heights"
+                        className="h-16"
+                    >
+                        <div className="my-4 flex flex-col items-center justify-center">
+                            <ArrowUpCircleIcon className="h-6" />
+                            <span>Height</span>
+                        </div>
+                    </Button>
+                    <Button
+                        color={color(['/bodies'])}
+                        variant="light"
+                        as={Link}
+                        href="/bodies"
+                        className="h-16"
+                    >
+                        <div className="my-4 flex flex-col items-center justify-center">
+                            <ArrowsRightLeftIcon className="h-6" />
+                            <span>Bodies</span>
+                        </div>
+                    </Button>
+                    <Button
+                        color={color(['/settings'])}
+                        variant="light"
+                        as={Link}
+                        href="/settings"
+                        className="h-16"
+                    >
+                        <div className="my-4 flex flex-col items-center justify-center">
+                            <Cog6ToothIcon className="h-6" />
+                            <span>Settings</span>
+                        </div>
+                    </Button>
                 </nav>
             </div>
         </Card>
