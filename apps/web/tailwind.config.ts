cb682645ccf5ee070680289ed5c76dbe3d374879
Sam Svindland <sam@Sams-MacBook-Pro.local>
2024-05-31 19:48:47 -0400

    remove theme

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -11,50 +11,7 @@ export default {
     plugins: [
         require('tailwindcss-safe-area'),
         nextui({
-            themes: {
-                light: {},
-                dark: {
-                    colors: {
-                        content1: '#154255',
-                        content2: '#154255',
-                        content3: '#154255',
-                        content4: '#154255',
-                        background: '#0D3140',
-                        foreground: '#F7C619',
-                        primary: {
-                            900: '#0D3140',
-                            800: '#0F3C51',
-                            700: '#154255',
-                            600: '#1D5A6F',
-                            500: '#28827A',
-                            400: '#3C9F8C',
-                            300: '#6FB6B0',
-                            200: '#A6D0C9',
-                            100: '#D4E7E1',
-                            50: '#F2F8F7',
-                            DEFAULT: '#28827A',
-                            foreground: '#0D3140',
-                        },
-                        success: {
-                            500: '#07B561',
-                            DEFAULT: '#07B561',
-                            foreground: '#fff',
-                        },
-                        warning: {
-                            500: '#FFD400',
-                            DEFAULT: '#FFD400',
-                            foreground: '#0D3140',
-                        },
-                        danger: {
-                            500: '#DB255E',
-                            DEFAULT: '#DB255E',
-                            foreground: '#fff',
-                        },
-                        focus: '#F7C619',
-                        secondary: '#AFD257',
-                    },
-                },
-            },
+            themes: {},
         }),
     ],
 } as Config;
